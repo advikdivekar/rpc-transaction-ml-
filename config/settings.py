992e9d633d09e8552bc84e12291bc109efb2e498
Vexx <advikdivekar@gmail.com>
2026-03-04 15:33:51 +0530

    chore: remove Alchemy RPC provider from settings.

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ CHAIN_ID = 11155111  # Sepolia
 # The RPC List (Single Source of Truth)
 RPC_PROVIDERS = {
     "infura": "https://sepolia.infura.io/v3/f6dccf73ccd64c06a5e7734325927bb9",
-    "alchemy": "https://eth-sepolia.g.alchemy.com/v2/demo",
     "drpc": "https://sepolia.drpc.org",
     "publicnode": "https://ethereum-sepolia-rpc.publicnode.com"
 }
